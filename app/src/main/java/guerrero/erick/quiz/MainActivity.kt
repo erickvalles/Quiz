f31da75d03dc319032d306b78d66db55aef4aa8f
Erick Guerrero <erickg@valles.udg.mx>
2022-09-08 11:55:31 -0500

    Agregado viewModel

--- app/src/main/java/guerrero/erick/quiz/MainActivity.kt
+++ app/src/main/java/guerrero/erick/quiz/MainActivity.kt
@@ -2,29 +2,28 @@ package guerrero.erick.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.viewModels
 import com.google.android.material.snackbar.Snackbar
 import guerrero.erick.quiz.databinding.ActivityMainBinding
 
-
+private const val TAG="MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val bancoPreguntas = listOf(
-        Pregunta(R.string.pregunta_chocolate,false),
-        Pregunta(R.string.pregunta_33,false),
-        Pregunta(R.string.pregunta_bajas, false),
-        Pregunta(R.string.pregunta_proyecto, true)
-    )
-    private var indice = 0
+    private val quizViewModel:QuizViewModel by viewModels()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Log.d(TAG, "Tengo un viewModel: ${quizViewModel}")
 
         binding.trueButton.setOnClickListener { view: View ->
 
@@ -36,19 +35,34 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.nextButton.setOnClickListener { view:View ->
-            indice = (indice + 1 ) % bancoPreguntas.size
+            quizViewModel.siguientePregunta()
             updateQuestion()
         }
         updateQuestion()
     }
 
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"En el onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"En el onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"En el onDestroy")
+    }
+
     private fun updateQuestion(){
-        val preguntaTextResId = bancoPreguntas[indice].textoPregunta
+        val preguntaTextResId = quizViewModel.currentQuestionText
         binding.questionText.setText(preguntaTextResId)
     }
 
     private fun checkAnswer(userAnswer:Boolean, view:View){
-        val correctAnswer = bancoPreguntas[indice].respuesta
+        val correctAnswer = quizViewModel.currentQuestionAnswer
         val messageResId = if (userAnswer == correctAnswer){
             R.string.correct_toast
         }else{
---
+++ app/src/main/java/guerrero/erick/quiz/QuizViewModel.kt
@@ -0,0 +1,29 @@
+package guerrero.erick.quiz
+
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+
+private const val TAG = "QuizViewModel"
+private const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+class QuizViewModel(private val savedStateHandle:SavedStateHandle): ViewModel() {
+    private val bancoPreguntas = listOf(
+        Pregunta(R.string.pregunta_chocolate,false),
+        Pregunta(R.string.pregunta_33,false),
+        Pregunta(R.string.pregunta_bajas, false),
+        Pregunta(R.string.pregunta_proyecto, true)
+    )
+    private var indice:Int
+        get() = savedStateHandle.get(CURRENT_INDEX_KEY) ?: 0
+        set(value) = savedStateHandle.set(CURRENT_INDEX_KEY,value)
+
+    val currentQuestionAnswer:Boolean
+        get() = bancoPreguntas[indice].respuesta
+    val currentQuestionText:Int
+        get() = bancoPreguntas[indice].textoPregunta
+
+    fun siguientePregunta(){
+        indice = (indice+1)%bancoPreguntas.size
+    }
+}
